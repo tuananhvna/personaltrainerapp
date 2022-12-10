235ee354aaa3a0f523c60e77f201f045193e6c91
Tuan Anh Nguyen <tuananhnguyen@Tuans-MacBook-Air.local>
2022-12-10 16:58:56 +0200

    add csv export for task 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
 
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <AppBar position="static">
           <Toolbar>
             <Typography
